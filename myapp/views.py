a20efd3e93e17cfcef81e2499dc5ed9cf08be59c
Eldor Karimov <hacktivist19021@gmail.com>
2024-01-09 10:37:51 +0500

    some changed

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. 0fd492d4 is the polls index.")
+    return render(request, 'index.html')
